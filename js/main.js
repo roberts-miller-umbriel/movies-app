37a7169aa1d4c8626fe3758590ef11bc28bd3e35
Dalton Miller <dalton.kye.miller@gmail.com>
2022-06-08 09:33:45 -0500

    feat(movie search)[WIP]: movie search

--- js/main.js
+++ js/main.js
@@ -8,13 +8,7 @@ export const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/original';
     const MOVIE_API_URL = 'https://tricky-excessive-booklet.glitch.me/movies';
     const TMDB_API_URL = 'https://api.themoviedb.org/3';
 
-    function getMovieData() {
-        // return fetch(MOVIE_API_URL, {
-        //     method: 'GET'
-        // })
-        //     .then(res => res.json())
-        //     .then(movieData => movieData)
-        //     .catch(error => console.error(error));
+    function getPopularMovies() {
         return fetch(`${TMDB_API_URL}/movie/popular?api_key=${MOVIE_DB_API_KEY}`, {
             method: 'GET',
         })
@@ -24,7 +18,26 @@ export const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/original';
 
     }
 
-    function addMovie({ title, director, rating, genre }) {
+    function getNowPlayingMovies() {
+        return fetch(`${TMDB_API_URL}/movie/now_playing?api_key=${MOVIE_DB_API_KEY}`, {
+            method: 'GET',
+        })
+            .then(res => res.json())
+            .then(data => data.results)
+            .catch(error => console.error(error));
+    }
+
+    function movieSearch(title) {
+        return fetch(`${TMDB_API_URL}/search/movie/?query=${encodeURIComponent(title)}&api_key=${MOVIE_DB_API_KEY}`, {
+            method: 'GET',
+        })
+            .then(res => res.json())
+            .then(data => data.results)
+            .catch(error => console.error(error));
+
+    }
+
+    function addCustomMovie({ title, director, rating, genre }) {
         return fetch(MOVIE_API_URL, {
             method: 'POST',
             headers: {
@@ -37,7 +50,7 @@ export const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/original';
             .catch(error => console.error(error));
     }
 
-    function updateMovie({ id, title, director, rating, genre }) {
+    function updateCustomMovie({ id, title, director, rating, genre }) {
         return fetch(`${MOVIE_API_URL}/${id}`, {
             method: 'PUT',
             headers: {
@@ -50,7 +63,7 @@ export const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/original';
             .catch(error => console.error(error));
     }
 
-    function deleteMovie(id) {
+    function deleteCustomMovie(id) {
         return fetch(`${MOVIE_API_URL}/${id}`, {
             method: 'DELETE',
             headers: {
@@ -63,23 +76,9 @@ export const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/original';
 
     }
 
-    getMovieData().then(movies => {
-        console.log(movies);
+    function renderMovieCarousel(movies, carouselLabel) {
         const moviesContainer = document.querySelector('#movies-container');
-        moviesContainer.appendChild(GenreColumn(movies));
-        console.log(movies);
-
-        // const uniqueGenres = movies
-        //     .map(movie => movie.genre)
-        //     .reduce((acc, curr) => {
-        //         if (!acc.includes(curr)) return [...acc, curr];
-        //         else return acc;
-        //     }, []);
-        //
-        // for (const genre of uniqueGenres) {
-        //     const moviesWithGenre = movies.filter(movie => movie.genre === genre);
-        //     moviesContainer.appendChild(GenreColumn(moviesWithGenre));
-        // }
+        moviesContainer.appendChild(GenreColumn(movies, carouselLabel));
 
 
         const genreContainers = document.querySelectorAll('.genre-carousel');
@@ -94,8 +93,27 @@ export const TMDB_IMG_URL = 'https://image.tmdb.org/t/p/original';
             });
 
         }
+    }
 
 
+    getPopularMovies().then(movies => {
+        renderMovieCarousel(movies, 'Popular');
     });
+    getNowPlayingMovies().then(movies => {
+        renderMovieCarousel(movies, 'Now Playing');
+    });
+
+
+    document.querySelector('#movie-search')
+        .addEventListener('submit', (e) => {
+            e.preventDefault();
+            const form = e.target;
+            const text = form.querySelector('input[type="text"]').value;
+            movieSearch(text)
+                .then(results => {
+                    console.log(results);
+                });
+
+        });
 
 })();
